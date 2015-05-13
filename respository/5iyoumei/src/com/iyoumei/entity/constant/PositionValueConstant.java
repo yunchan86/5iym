44882c42ee03c81ab72afa7243c0052661d27ac9
chy <yunchan86@163.com>
2015-05-13 22:47:48 +0800

    增加并修改收益的部分流程的业务处理

---
+++ respository/5iyoumei/src/com/iyoumei/entity/constant/PositionValueConstant.java
@@ -0,0 +1,7 @@
+package com.iyoumei.entity.constant;
+
+public class PositionValueConstant {
+
+	public final static byte LEFT = 0 ;
+	public final static byte RIGHT = 0 ;
+}
---
+++ respository/5iyoumei/src/com/iyoumei/entity/constant/UserRewardRuleConstant.java
@@ -0,0 +1,8 @@
+package com.iyoumei.entity.constant;
+
+public class UserRewardRuleConstant {
+
+	public final static String STATUS_OK = "1" ;
+	public final static String STATUS_DEFAULT = "0" ;
+	public final static String STATUS_INVALID = "2" ;
+}
